6450b9a9b0f7806233b690dc6f7fa97a558d262f
Medusas71 <medusas71@icloud.com>
2022-06-18 11:04:58 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from .models import Product, Category
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -41,7 +42,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
